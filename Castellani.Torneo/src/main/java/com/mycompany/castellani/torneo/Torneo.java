04e8ea9a578e1085f5b6f35b7f4562fa2c6c4db8
Alessandro-Castellani <alecastellani332@gmail.com>
2024-12-02 21:59:15 +0100

    Primo commit

---
+++ Castellani.Torneo/src/main/java/com/mycompany/castellani/torneo/Torneo.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.castellani.torneo;
+
+/**
+ *
+ * @author user
+ */
+public class Torneo {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
